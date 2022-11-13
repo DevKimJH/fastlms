8f3dbb216fa482d160bc3d25736dceb69b1637c5
DevKimJH <hg116710@naver.com>
2022-11-13 20:59:38 +0900

    fastlms 초기화 설정-2

--- src/main/java/com/zerobase/fastlms/FastlmsApplication.java
+++ src/main/java/com/zerobase/fastlms/FastlmsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FastlmsApplication {
 
+    // 2021.08.23
+
     public static void main(String[] args) {
         SpringApplication.run(FastlmsApplication.class, args);
     }
