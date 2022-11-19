e1f7d72be5bfe450a9604a1e67d1809820470064
DevKimJH <hg116710@naver.com>
2022-11-19 22:55:06 +0900

    회원 검색 기능 구현

--- src/main/java/com/zerobase/fastlms/admin/mapper/MemberMapper.java
+++ src/main/java/com/zerobase/fastlms/admin/mapper/MemberMapper.java
@@ -2,6 +2,7 @@ package com.zerobase.fastlms.admin.mapper;
 
 
 import com.zerobase.fastlms.admin.dto.MemberDto;
+import com.zerobase.fastlms.admin.model.MemberParam;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,6 +10,6 @@ import java.util.List;
 @Mapper
 public interface MemberMapper {
 
-    List<MemberDto> selectList(MemberDto parameter);
+    List<MemberDto> selectList(MemberParam parameter);
 
 }
